0157ebc4d167abe9b1bd2e9602cdb196637e0acb
Andre Sousa lajas <asousa-l@c1r17s1.42lisboa.com>
2022-01-23 13:53:42 +0000

    a

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -9,30 +9,25 @@
 /*   Updated: 2022/01/18 13:54:04 by asousa-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
-#include <stdbool.h>
-#include <stdio.h>
-
 char	*ft_strcapitalize(char *str)
 {
 	int		i;
-	int	up;
+	int		next;
+	char	c;
 
 	i = 0;
-	up = 1;
-	while (str[i] != '\0')
+	next = 1;
+	while (*(str + i) != '\0')
 	{
-		if ((str[i] >= 'a' && str[i] < 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 0 && str[i] <= '9'))
-		{
-			if (up && (str[i] >= 'a' && str[i] < 'z'))
-				str[i] = (str[i] - 32);
-			else if (!up && (str[i] >= 'a' && str[i] < 'z'))
-				str[i] = (str[i] + 32);
-			up = 0;
-		}
+		c = *(str + i);
+		if (next == 1 && c >= 'a' && c <= 'z')
+			str[i] -= 32;
+		else if (next == 0 && c >= 'A' && c <= 'Z')
+			str[i] += 32;
+		if (c < '0' || (c > '9' && c < 'A') || (c > 'Z' && c < 'a') || c > 122)
+			next = 1;
 		else
-			upper = 1;
+			next = 0;
 		i++;
 	}
 	return (str);
